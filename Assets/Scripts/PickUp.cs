1d5e8161b927b4bb33442f16e2641902d1580e22
itamarc101 <45041126+itamarc101@users.noreply.github.com>
2024-03-09 21:20:45 +0200

    Update PickUp.cs

--- Assets/Scripts/PickUp.cs
+++ Assets/Scripts/PickUp.cs
@@ -7,6 +7,7 @@ public class PickUp : MonoBehaviour
     public Transform holdSpot;
     public LayerMask pickupMask;
     private GameObject item;
+    [SerializeField] private float offset = 0.4f;
 
     private InputMover playerInput;
 
@@ -33,7 +34,7 @@ public class PickUp : MonoBehaviour
             else
             {
                 Debug.Log("Entered ELSE");
-                Collider2D pickUp = Physics2D.OverlapCircle(transform.position + playerInput.FacingDirection, 0.4f, pickupMask);
+                Collider2D pickUp = Physics2D.OverlapCircle(transform.position + playerInput.FacingDirection, offset, pickupMask);
                 if (pickUp)
                 {
                     item = pickUp.gameObject;
